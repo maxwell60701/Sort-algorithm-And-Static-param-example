97a900e0ad999729811fd452d352f58b0825060a
maxwell <1007499575@qq.com>
2018-08-07 15:33:21 +0800

    。。。

---
+++ ClassLibrary1/Sort/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public class SortMethod
+    {
+        private int privot;
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public int[] Bubble(int[] list)
+        {
+            for (int i = 0; i < list.Length - 1; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    // Exchange(ListPrev[j], ListPrev[j + 1]);
+                    if (list[j] > list[j + 1])
+                    {                    
+                        Swap(j, j + 1, list);
+                    }
+                }
+
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 快速排序
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public void QuickSort(ref int[] list, int low, int high)
+        {
+            if (low < high)
+            {
+                privot = Partion(list, low, high);
+                if (privot != low)
+                    QuickSort(ref list, low, privot - 1);   //对中间值左边比他小的数组排序
+                if (privot != high)
+                    QuickSort(ref list, privot + 1, high); //对中间值右边比他大的数组排序
+            }
+
+        }
+        private int Partion(int[] list, int low, int high)
+        {
+            int pviot = list[low];
+            while (low < high)
+            {
+                while (low < high && list[high] > pviot)
+                    high--;
+                Swap(low, high, list);
+
+
+                while (low < high && list[low] <= pviot)
+                    low++;
+                Swap(low, high, list);
+            }
+            return low;
+        }
+        /// <summary>
+        /// 交换LOW与HIGH的值
+        /// </summary>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <param name="list"></param>
+        private void Swap(int low,int high,int[] list)
+        {
+            int mid = 0;
+            mid = list[high];
+            list[high] = list[low];
+            list[low] = mid;
+
+        }
+
+    }
+}
---
+++ ClassLibrary1/Sort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortMethod sort1 = new SortMethod();
+            var list = new int[] { 10, 8, 5, 9, 7, 9, 7, 1, 3, 23, 15, 65, 23, 21, 10, 6 };
+            var list2 = list;
+
+            Console.WriteLine("冒泡排序结果为：" + string.Join(",", sort1.Bubble(list)));
+            sort1.QuickSort(ref list2, 0, 15);
+            Console.WriteLine("快速排序结果为：" + string.Join(",", list2));
+            Console.ReadKey();
+        }
+    }
+}
